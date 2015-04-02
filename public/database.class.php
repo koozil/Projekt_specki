cc67d4ed7ee1de15723a408a8390d69461ecf2d4
Marcin Pyrka <pyrka.marcin@gmail.com>
2015-04-02 06:42:33 +0000

    Update database.class.php

--- public/database.class.php
+++ public/database.class.php
@@ -4,7 +4,7 @@ class Database
 {
     
     protected static $_instancja;
-    protected static $_prefiks;
+    protected static $_prefix;
     protected $_mysqli;
     protected $_zapytanie;
     
@@ -15,7 +15,6 @@ class Database
     protected $port;
     protected $kodowanie;
     
-    protected $czyzapytanie;
     public $polaczenie;
     
     public function __construct($host = NULL, $nazwauzytkownik = NULL, $haslo = NULL, $nazwabazadanych = NULL, $port = NULL, $kodowanie = 'utf8')
@@ -35,18 +34,37 @@ class Database
             $this->nazwabazadanych = $nazwabazadanych;
             $this->port            = $port;
             $this->kodowanie       = $kodowanie;
-            if ($czyzapytanie) {
-                $this->czyzapytanie = true;
-                return;
-            }
             
             if (!is_object($host))
                 $this->polaczenie();
-            $this->ustawprefiks();
+            $this->ustawprefix();
             self::$_instance = $this;
         }
     }
-    public function ustawprefiks()
+    public function ustawprefix()
     {
+        self::$_prefix = $prefix;
+        return $this;
+    }
+    
+    public function connect()
+    {
+        if (empty($this->host)) {
+            die('Brak ustawień bazy danych.');
+        }
+        $this->_mysqli = new mysqli($this->host, $this->nazwauzytkownik, $this->haslo, $this->nazwabazadanych, $this->port) or die('Wystąpił problem z połączeniem z bazą danych.');
+        if ($this->kodowanie) {
+            $this->_mysqli->set_charset($this->kodowanie);
+        }
+    }
+    
+    public function zapytanie($zapytanie, $iloscwierszy = null)
+    {
+        $this->_zapytanie = filter_var($zapytanie, FILTER_SANITIZE_STRING);
+        $stmt             = $this->_buildQuery($iloscwierszy);
+        $stmt->execute();
+        $this->_stmtError = $stmt->error;
+        $this->reset();
+        return $this->_dynamicBindResults($stmt);
     }
 }
