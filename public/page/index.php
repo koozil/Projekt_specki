2364a82fe0fee0b5e9ecd64fefbe2d45329ca5fb
Marcin Pyrka <pyrka.marcin@gmail.com>
2015-04-02 07:50:46 +0200

    Update index.php

--- public/page/index.php
+++ public/page/index.php
@@ -1,5 +1,4 @@
-
-    <div class="container-fluid">
+<div class="container-fluid">
       <div class="row">
         <div class="col-sm-3 col-md-2 sidebar">
           <ul class="nav nav-sidebar">
@@ -110,15 +109,3 @@
           </div>
         </div>
       </div>
-
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-    <script src="dist/js/bootstrap.min.js"></script>
-    <!-- Just to make our placeholder images work. Don't actually copy the next line! -->
-    <script src="../assets/js/vendor/holder.js"></script>
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
-  </body>
-</html>
