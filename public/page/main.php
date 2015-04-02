96c2500ab3a08908d35c214b13167b6c794d905f
Marcin Pyrka <pyrka.marcin@gmail.com>
2015-04-02 07:44:22 +0200

    Update main.php

--- public/page/main.php
+++ public/page/main.php
@@ -1,6 +1,4 @@
-<?php
-
-    <div class="container-fluid">
+<div class="container-fluid">
       <div class="row">
         <div class="col-sm-3 col-md-2 sidebar">
           <ul class="nav nav-sidebar">
