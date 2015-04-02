15a372abbb50b2758a6f72c4095eb28f3619ab01
Marcin Pyrka <pyrka.marcin@gmail.com>
2015-04-02 06:08:26 +0000

    REF

--- public/about.php
+++ public/about.php
@@ -1,13 +1,13 @@
 <?php
-  include 'nav.php';
-  include 'database.class.php';
-  
-  ob_start();
-  
-  include 'page/nav.php';
-  
-  
-  include 'page/about.php';
-  include 'page/footer.php';
-  
-  ob_end_flush();
+include 'nav.php';
+include 'database.class.php';
+
+ob_start();
+
+include 'page/nav.php';
+
+
+include 'page/about.php';
+include 'page/footer.php';
+
+ob_end_flush();
