2b244fe49a52aa71975d398672519f2c47aef85b
Filip Kozłowski <kooziloo@gmail.com>
2015-04-13 13:11:10 +0200

    no coś z tą bazą będzie.
    kiedyś

--- public/add.php
+++ public/add.php
@@ -1,12 +1,21 @@
 <?php
 include 'database.class.php';
-
 ob_start();
-
 include 'page/nav.php';
+
+$username="root";
+$password="";
+$database="cats";
+$imie=$_POST['imię'];
+$rasa=$_POST['rasa'];
+$wiek=$_POST['wiek'];
+$cena=$_POST['cena'];
+
+
 
 
 include 'page/list.php';
 include 'page/footer.php';
 
 ob_end_flush();
+?>
